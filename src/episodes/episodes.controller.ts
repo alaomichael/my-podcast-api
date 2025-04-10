827f825ff62732a0a6ef1b2d1003f86eacbb218b
Michael Alao <contactleomax@gmail.com>
2025-04-10 21:07:27 +0100

    first commit

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('episodes')
 export class EpisodesController {
     @Get()
-    findAll() {
-        return 'This action returns all episodes';
+    findAll(@Query('page') page: number, @Query('limit') limit: number, @Query('sort') sort: 'asc' | 'desc') {
+        if (page && limit && sort) return `This action returns all episodes with page ${page}, limit ${limit}, and sort ${sort}`;
+        return `This action returns all episodes with page ${page}, limit ${limit}, and sort ${sort}`;
+    }
+
+    @Get('featured')
+    findFeatured() {
+        return 'This action returns all featured episodes';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action returns a #${id} episode`;
     }
 
     @Post()
